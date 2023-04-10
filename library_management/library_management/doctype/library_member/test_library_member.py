57151107226b207999d2dceb7eb0155cd855ef7e
Edward-Kabue <ekabue0@gmail.com>
2023-04-10 10:36:58 +0300

    commit

---
+++ library_management/library_management/doctype/library_member/test_library_member.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, edward and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibraryMember(FrappeTestCase):
+	pass
